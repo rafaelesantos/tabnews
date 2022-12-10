4451f86133f6cfaccf9b993fccbbc8ee2af1f20a
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 23:57:45 -0400

    Add interactor on init content to consume API

---
+++ Presentation/Interactors/InitContentInteractor.swift
@@ -0,0 +1,30 @@
+//
+//  InitContentInteractor.swift
+//  Presentation
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import Foundation
+import Domain
+import Data
+
+public protocol InitContentInteractorProtocol {
+    func getInitContent() async throws -> [InitContentResponse]
+}
+
+public final class InitContentInteractor: InitContentInteractorProtocol {
+    private let useCase: GetInitContent
+    
+    public init(useCase: GetInitContent) {
+        self.useCase = useCase
+    }
+    
+    public func getInitContent() async throws -> [InitContentResponse] {
+        let result = await useCase.get()
+        switch result {
+        case .success(let response): return response
+        case .failure(let error): throw error
+        }
+    }
+}
