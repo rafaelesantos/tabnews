7192e2851793a4fd931bf59eb2c688f8c3e1389f
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 23:58:05 -0400

    Create init content router protocol

---
+++ Presentation/RoutersProtocol/InitContentRouterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  InitContentRouterProtocol.swift
+//  Presentation
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import Foundation
+
+public protocol InitContentRouterProtocol {
+    
+}
