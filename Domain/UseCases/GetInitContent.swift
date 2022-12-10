5ce740a53a7fd279ec83c557229f675cc000911e
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 22:55:04 -0400

    Add GetInitContent use case

---
+++ Domain/UseCases/GetInitContent.swift
@@ -0,0 +1,13 @@
+//
+//  GetInitContent.swift
+//  Domain
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import Foundation
+
+public protocol GetInitContent {
+    typealias Result = Swift.Result<InitContentResponse, TabNewsError>
+    func get(completion: @escaping (Result) -> Void)
+}
