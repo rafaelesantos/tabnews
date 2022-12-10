9369bd64e784885f1672d65db2efaba8cf497f17
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 23:10:28 -0400

    Create a custom type to http network adapter

---
+++ Infrastructure/Helpers/Typealias.swift
@@ -0,0 +1,11 @@
+//
+//  Typealias.swift
+//  Infrastructure
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import Foundation
+import RefdsInfra
+
+public typealias TabNewsNetworkAdapter = HttpNetworkAdapter
