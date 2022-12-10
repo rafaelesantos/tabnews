ad1f5379e6eb0cb5f16b0941db8344e7757cab00
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 22:36:28 -0400

    Create xcode project

---
+++ TabNews/TabNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabNewsApp.swift
+//  TabNews
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TabNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
