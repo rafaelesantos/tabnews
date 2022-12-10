5a362ea20cf85c1277773b065d09b872692102c4
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 23:06:56 -0400

    Make custom types to data layer

---
+++ Data/Helpers/Typealias.swift
@@ -0,0 +1,17 @@
+//
+//  Typealias.swift
+//  Data
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import Foundation
+import RefdsData
+
+public typealias TabNewsHttpScheme = HttpScheme
+public typealias TabNewsHttpEndpoint = HttpEndpoint
+public typealias TabNewsHttpRequest = HttpRequest
+public typealias TabNewsHttpClient = HttpClient
+public typealias TabNewsHttpHeader = HttpHeader
+public typealias TabNewsHttpError = HttpError
+
