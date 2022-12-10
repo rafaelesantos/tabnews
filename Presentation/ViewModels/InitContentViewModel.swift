ecfbcc46473e8bffb1f9c37eba56f31297969a49
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-09 23:57:11 -0400

    Add init content view model to use on presenter

---
+++ Presentation/ViewModels/InitContentViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  InitContentViewModel.swift
+//  Presentation
+//
+//  Created by Rafael Santos on 09/12/22.
+//
+
+import Foundation
+
+public struct InitContentViewModel {
+    let title: String
+    let updated_at: Date
+    let tabcoins: Int?
+    let owner_username: String?
+}
