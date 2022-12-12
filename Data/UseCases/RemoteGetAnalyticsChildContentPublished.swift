8515a4c6ed2e37750fa36e23aa5a86350da19f9b
Rafael Escaleira <rafael.escaleira@g.globo>
2022-12-12 01:20:28 -0400

    Add more app icons sizes

---
+++ Data/UseCases/RemoteGetAnalyticsChildContentPublished.swift
@@ -0,0 +1,34 @@
+//
+//  RemoteGetAnalyticsChildContentPublished.swift
+//  Data
+//
+//  Created by Rafael Santos on 11/12/22.
+//
+
+import Foundation
+import Domain
+
+public final class RemoteGetAnalyticsChildContentPublished: GetAnalyticsChildContentPublished, TabNewsHttpRequest {
+    public typealias Response = [AnalyticsChildContentPublishedResponse]
+    public var httpClient: TabNewsHttpClient
+    public var httpEndpoint: TabNewsHttpEndpoint
+    
+    public init(httpClient: TabNewsHttpClient, httpEndpoint: TabNewsHttpEndpoint) {
+        self.httpClient = httpClient
+        self.httpEndpoint = httpEndpoint
+    }
+    
+    public func getAnalyticsChildContentPublished() async -> GetAnalyticsChildContentPublished.Result {
+        let result = await httpClient.request(self)
+        switch result {
+        case .success(let response): return .success(response)
+        case .failure(let httpError): return .failure(.requestError(error: httpError))
+        }
+    }
+    
+    public func decode(_ data: Data, endpoint: TabNewsHttpEndpoint?) throws -> Response {
+        let jsonDecoder = JSONDecoder()
+        guard let decoded: Response = try? jsonDecoder.decode(Response.self, from: data) else { throw TabNewsError.decodedError(type: Response.self) }
+        return decoded
+    }
+}
